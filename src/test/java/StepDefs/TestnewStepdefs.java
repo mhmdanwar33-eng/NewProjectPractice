f6bbbc88693c7ea29de1284a7a104fdb5e22d234
mhmdanwar33-eng <mhmdanwar33@gmail.com>
2025-10-23 07:55:43 +0300

    default

---
+++ src/test/java/StepDefs/TestnewStepdefs.java
@@ -0,0 +1,29 @@
+import com.ecommerce.pages.HomePage;
+import com.ecommerce.utils.DriverFactory;
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class TestnewStepdefs {
+
+    private WebDriver driver;
+    HomePage homePage;
+    public TestnewStepdefs(){
+        this.driver = DriverFactory.getDriver();
+        this.homePage = new HomePage(driver);
+
+    }
+
+    @Given("user click url")
+    public void userClickUrl() {
+        homePage.OpenHomePage();
+    }
+
+    @Then("home page opened")
+    public void homePageOpened() {
+        Assert.assertEquals(homePage.pageTitle(),"Online Shopping & Wholesale Marketplace in MENA | Tradeling");
+    }
+}
